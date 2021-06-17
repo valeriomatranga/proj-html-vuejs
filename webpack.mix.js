b2646bc77e07bc7af96efc200515eac6d7a4efbc
valeriomatranga <78118886+valeriomatranga@users.noreply.github.com>
2021-06-17 14:58:46 +0200

    correzzione percorso file img

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,7 @@ let mix = require('laravel-mix');
 
 mix
 .js('src/app.js', 'js')
-.sass('src/app.scss', 'css').options({processCssUrls:false})
+.sass('src/app.scss', 'css')
+.options({processCssUrls:false})
 .copyDirectory('src/img', 'dist/img')
 .setPublicPath('dist');
